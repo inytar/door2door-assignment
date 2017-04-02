3a6f22847d92929b0bd765e9d5fcff9b2579403f
inytar <pietpiet@fastmail.net>
2017-04-02 17:12:57 +0200

    Basic functionality test of app

--- d2d/tests/test_app.py
+++ d2d/tests/test_app.py
@@ -12,4 +12,25 @@ def test_root():
     root = client.get('/')
 
     assert root.status_code == 200
-    assert len(root.json['greetings']) == 2
+    assert b'id="map"' in root.body
+
+
+def test_data():
+    morepath.scan(d2d)
+    morepath.commit(d2d.App)
+
+    client = Client(d2d.App())
+    response = client.post('/')
+
+    assert response.status_code == 200
+    assert 'crs' in response.json
+    assert response.json['type'] == 'FeatureCollection'
+    features = response.json['features']
+    feature = features[0]
+    assert feature['type'] == 'Feature'
+    assert 'geometry' in feature
+    properties = feature['properties']
+    assert 'probability' in properties
+    geometry = feature['geometry']
+    assert geometry['type'] == 'Point'
+    assert 'coordinates' in geometry
